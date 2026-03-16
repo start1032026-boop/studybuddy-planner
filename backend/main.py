ab27a4c45af799a12e7d19ec22c0a0b8f17cfeb2
Start1032026-boop <start1032026@gmail.com>
2026-03-16 17:30:15 +0100

    backend CORS added

--- backend/main.py
+++ backend/main.py
@@ -1,14 +1,29 @@
 from fastapi import FastAPI
+from pydantic import BaseModel
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
 
+origins = ["http://localhost:3000"]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+class StudyInput(BaseModel):
+    subjects: list[str]
+    hours_per_day: int
+
 @app.post("/generate-plan")
-def generate_plan(data: dict):
-    subjects = data["subjects"]
-    
+def generate_plan(data: StudyInput):
+
     plan = {
-        "Monday": subjects,
-        "Tuesday": subjects
+        "Monday": data.subjects[:2],
+        "Tuesday": data.subjects[::-1][:2]
     }
 
     return {"plan": plan}
